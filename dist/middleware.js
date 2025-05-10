84f5c43be553d78607bedc890ba562d2c87fde2b
Sameer <sameer.edu19@gmail.com>
2025-05-10 14:53:40 +0530

    completed backend

---
+++ dist/middleware.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userMiddleware = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const JWT_USER_PASSWORD = "Sameer123";
+const userMiddleware = (req, res, next) => {
+    const header = req.headers["authorization"];
+    const decoded = jsonwebtoken_1.default.verify(header, JWT_USER_PASSWORD);
+    if (decoded) {
+        // @ts-ignore
+        req.userId = decoded.id;
+    }
+    else {
+        res.json({
+            message: "you are not logged in"
+        });
+    }
+};
+exports.userMiddleware = userMiddleware;
